9e58067f48b7a49e9b6b3ccb3b880921127feadc
NavyaSriB <basava@thoughtworks.com>
2016-12-01 11:24:45 +0530

    [Sooraj/Navya] resolve repl and add example for usage of behavior

--- repl.js
+++ repl.js
@@ -1,15 +1,22 @@
 const repl = require('repl');
 const parser = require('./Parser.js')
 
-// const myTranslator = new Translator('en', 'fr');
-
 function myEval(cmd, context, filename, callback) {
-    var tree = parser.parse(cmd);
-    callback(null, tree.evaluate());
-    // callback(null, myTranslator.translate(cmd));
+    try {
+      var tree = parser.parse(cmd);
+      var output = tree[0].evaluate();
+    } catch(err) {
+      return callback(new repl.Recoverable(err));
+    }
+    callback(null, output);
 }
 
-repl.start({
-    prompt: '> ',
+var start = repl.start({
+    prompt: 'nave > ',
     eval: myEval
+});
+
+start.on('exit',function(){
+  console.log('Did you like nave....????');
+  process.exit();
 });
